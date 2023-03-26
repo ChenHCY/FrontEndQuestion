ff8b6aaade72952b79a752e67e1d81966729553c
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-03-26 02:09:49 -0700

    Create 123 Best Time to Buy and Sell Stock III.js

---
+++ JS Leetcode Question/123 Best Time to Buy and Sell Stock III.js
@@ -0,0 +1,58 @@
+/* Leetcode 123. Best Time to Buy and Sell Stock III
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+Find the maximum profit you can achieve. You may complete at most two transactions.
+
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+Example 1:
+
+Input: prices = [3,3,5,0,0,3,1,4]
+Output: 6
+Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+
+Example 2:
+
+Input: prices = [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are engaging multiple transactions at the same time. You must sell before buying again.
+
+Example 3:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 10^5
+0 <= prices[i] <= 10^5
+*/
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    if(prices.length == 1){
+        return 0;
+    }
+    
+    let buy1 = -prices[0];
+    let buy2 = -prices[0];
+    let sell1 = 0;
+    let sell2 = 0;
+
+    for(let i = 0; i < prices.length; i++){
+        buy1 = Math.max(buy1, -prices[i]);
+        sell1 = Math.max(sell1, buy1 + prices[i]);
+        buy2 = Math.max(buy2, sell1 - prices[i]);
+        sell2 = Math.max(sell2, buy2 + prices[i]);
+    }
+
+    return sell2;
+};
