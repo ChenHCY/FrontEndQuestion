2b05440bc66f107acacafeba3890415b48e5cdf4
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-05-15 17:54:02 -0700

    Create 2629 Function Composition.js

---
+++ Leetcode JavaScript NEW/2629 Function Composition.js
@@ -0,0 +1,82 @@
+/* Leetcode 2629. Function Composition
+
+Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.
+
+The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
+
+The function composition of an empty list of functions is the identity function f(x) = x.
+
+You may assume each function in the array accepts one integer as input and returns one integer as output.
+
+Example 1:
+
+Input: functions = [x => x + 1, x => x * x, x => 2 * x], x = 4
+Output: 65
+Explanation:
+Evaluating from right to left ...
+Starting with x = 4.
+2 * (4) = 8
+(8) * (8) = 64
+(64) + 1 = 65
+
+Example 2:
+
+Input: functions = [x => 10 * x, x => 10 * x, x => 10 * x], x = 1
+Output: 1000
+Explanation:
+Evaluating from right to left ...
+10 * (1) = 10
+10 * (10) = 100
+10 * (100) = 1000
+Example 3:
+
+Input: functions = [], x = 42
+Output: 42
+Explanation:
+The composition of zero functions is the identity function
+ 
+
+Constraints:
+
+-1000 <= x <= 1000
+0 <= functions.length <= 1000
+all functions accept and return a single integer
+*/
+
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+
+
+//Solution 1: 
+var compose = function(functions) {
+	return function(x) {
+        while(functions.length !== 0){
+            const fn = functions.pop(); //每次提取一个fn运算，从后往前
+            x = fn(x);
+            console.log(x);
+        }
+        return x;
+    }
+};
+
+//Solution 2： 
+var compose = function(functions) {
+	return function(x) {
+        if(!functions.length){
+            return x;
+        }
+
+        let res = x;
+        for(let i = functions.length - 1; i >= 0; i--){
+            res = functions[i](res);
+        }
+        
+        return res;
+    }
+};
+
+//test part:
+ const fn = compose([x => x + 1, x => 2 * x])
+ fn(4) // Output: 9
