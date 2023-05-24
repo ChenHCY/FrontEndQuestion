35d139181d54b7cf3ee01de4cbe897cfbe4ce23d
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-05-24 00:39:02 -0700

    Create 94 Binary Tree Inorder Traversal.js

---
+++ JS Leetcode Question/Binary Tree Question/94 Binary Tree Inorder Traversal.js
@@ -0,0 +1,56 @@
+/* Leetcode 94. Binary Tree Inorder Traversal
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+
+Example 2:
+
+Input: root = []
+Output: []
+
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ 
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    let res = [];
+    //recusrion function
+    inorder(root, res);
+    return res;
+};
+
+const inorder = (root, res) => {
+    if(root === null){
+        return;
+    }
+    //中序遍历：左 - 打印 - 右
+    inorder(root.left, res);
+    res.push(root.val);
+    inorder(root.right, res);
+}
