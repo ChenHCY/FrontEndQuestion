06f75e3dd6b6bde61ea3c7ad68eb4a174d96df83
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-06-22 19:29:36 -0700

    Create 543 Diameter of Binary Tree.js

---
+++ JS Leetcode Question/Binary Tree Question/543 Diameter of Binary Tree.js
@@ -0,0 +1,58 @@
+/* Leetcode 543. Diameter of Binary Tree
+
+Given the root of a binary tree, return the length of the diameter of the tree.
+
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+The length of a path between two nodes is represented by the number of edges between them.
+
+Example 1:
+Input: root = [1,2,3,4,5]
+Output: 3
+Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+
+Example 2:
+
+Input: root = [1,2]
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-100 <= Node.val <= 100
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function(root) {
+    let res = 0;
+
+    const countDiameter = (root) => {
+        //exit condition
+        if(!root){
+            return 0;
+        }
+        
+        const leftDepth = countDiameter(root.left);
+        const rightDepth = countDiameter(root.right);
+
+        res = Math.max(res, (leftDepth + rightDepth)); //整个BST二叉树的最大深度
+
+        return Math.max(leftDepth, rightDepth) + 1; //每个节点能产生的最大深度
+    }
+
+    countDiameter(root); //call and use the function to conut the depth of this Binary Search Tree
+
+    return res;
+};
