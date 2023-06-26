4e2bbec9206598dab9d5e2bfdbe3682b0184d887
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-06-25 18:58:20 -0700

    Create 144 Binary Tree Preorder Traversal.js

---
+++ JS Leetcode Question/Binary Tree Question/144 Binary Tree Preorder Traversal.js
@@ -0,0 +1,51 @@
+/* Leetcode 144. Binary Tree Preorder Traversal
+
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+
+Example 2:
+
+Input: root = []
+Output: []
+
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    let res = [];
+    preorder(root, res); //recursion function
+    return res;
+};
+
+const preorder = (root, res) => {
+    if(root === null){
+        return;
+    }
+    res.push(root.val);
+    preorder(root.left, res);
+    preorder(root.right, res);
+}
