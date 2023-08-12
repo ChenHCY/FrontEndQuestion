890d069afebd95f955bfae9142888db05f1dca08
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-08-11 18:46:04 -0700

    Create 81 Search in Rotated Sorted Array II.java

---
+++ JS Leetcode Question/Binary Search Question/81 Search in Rotated Sorted Array II.java
@@ -0,0 +1,69 @@
+/*Leetcode 81. Search in Rotated Sorted Array II
+
+There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+
+Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) 
+
+such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). 
+
+For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
+
+Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
+
+You must decrease the overall operation steps as much as possible.
+
+
+Example 1:
+
+Input: nums = [2,5,6,0,0,1,2], target = 0
+Output: true
+
+Example 2:
+
+Input: nums = [2,5,6,0,0,1,2], target = 3
+Output: false
+ 
+
+Constraints:
+
+1 <= nums.length <= 5000
+-10^4 <= nums[i] <= 10^4
+nums is guaranteed to be rotated at some pivot.
+-10^4 <= target <= 10^4
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while(left <= right){
+        let mid = left + Math.floor((right - left) / 2);
+
+        if(nums[mid] === target){
+            return true;
+        } 
+        //找到完全递增的区间
+        else if(nums[left] < nums[mid]){ 
+            if(nums[left] <= target && target < nums[mid]){
+                right = mid - 1;
+            } else{
+                left = mid + 1;
+            }
+        } else if(nums[left] > nums[mid]){
+            if(nums[mid] < target && target <= nums[right]){
+                left = mid + 1;
+            } else{
+                right = mid - 1;
+            }
+        } else { //nums[left] == nums[mid], 因为存在重复的数字，所以要去除，找到完全递增的区间range
+            left += 1;
+        } //For example: nums = [1,0,1,1,1] target = 0 ==> can not use if(nums[left] <= nums[mid])   
+    }
+
+    return false;   
+};
