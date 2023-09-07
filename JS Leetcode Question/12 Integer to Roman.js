1f21eaa09359b3aceb070f2facafec1a17c3a865
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-09-06 18:24:54 -0700

    Create 12 Integer to Roman.js

---
+++ JS Leetcode Question/12 Integer to Roman.js
@@ -0,0 +1,66 @@
+/* Leetcode 12. Integer to Roman
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given an integer, convert it to a roman numeral.
+
+Example 1:
+
+Input: num = 3
+Output: "III"
+Explanation: 3 is represented as 3 ones.
+
+Example 2:
+
+Input: num = 58
+Output: "LVIII"
+Explanation: L = 50, V = 5, III = 3.
+
+Example 3:
+
+Input: num = 1994
+Output: "MCMXCIV"
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ 
+
+Constraints:
+
+1 <= num <= 3999
+*/
+/**
+ * @param {number} num
+ * @return {string}
+ */
+
+var intToRoman = function(num) {
+    const val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const romanChar = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    
+    let res = "";
+
+    //因为罗马字母的排列，是从大到小的，所以如果能取到较大的字母值，首先放在左边
+    //compaere the num with every value from val(Roman character value)
+    for(let i in val){
+        while(num >= val[i]){
+            res += romanChar[i];
+            num -= val[i];
+        }
+    }
+
+    return res;
+
+};
