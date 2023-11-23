6c9cd4665f5d72ac27d54b9fa2ddcf73a62a14f2
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-11-22 18:43:22 -0800

    Update userLevel.js

--- TypeScript Leetcode Question/userLevel.js
+++ TypeScript Leetcode Question/userLevel.js
@@ -6,6 +6,43 @@ const userArr = [
   { User: 'user5', Level: 3 }
 ];
 
+// Solution 1: Not use reduce function  ***********************************************************
+// 1. travser all the element and add them toegther with same level
+let groupUsers = [];
+
+for(let i = 0; i < userArr.length; i++){
+  let currLevel = userArr[i].Level;
+
+  // 检查groupUsers是否已经存在了这个级别，如果是的，hasNot会直接得到对应的array，用在后面add 
+  const hasNot = groupUsers.find(item => item.level === currLevel);
+  
+  if(hasNot){
+    hasNot.users.push(userArr[i].User);
+  } else{
+    groupUsers.push({level: currLevel, users: [userArr[i].User]});
+  }
+}
+
+// 2. then we need sort all the element based on level increasing order
+const sortLevels = groupUsers.sort((a, b) => a.level - b.level);
+
+// 3. Display render the all level with each user
+sortLevels.forEach(groupItem => {
+  console.log(`Level ${groupItem.level}`); //依照格式输出
+  groupItem.users.forEach(user => {
+
+    let currNum = ""; //记录每个User的编号
+    for(let i = 0; i < user.length; i++){
+      if(!isNaN(Number(user[i]))){ //判断这个字符是不是数字
+        currNum += user[i];
+      }
+    }
+    console.log(`User ${currNum}`); //依照格式输出
+  })
+});
+
+
+// Solution 2: Used reduce function() 来根据条件 合并累积 ***********************************************
 // 1. travser all the element and add them toegther with same level
 const groupUsers = userArr.reduce((map, user) => {
   const currLevel = user.Level;
